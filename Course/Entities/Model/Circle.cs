a7d98fc395deb2725001578829664322c89d69de
LinaAmaral <lina.amaralcaetano@gmail.com>
2025-10-18 14:42:03 -0300

    Diferença entre herança e interface (classe shape abstrata)

---
+++ Course/Entities/Model/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities.Model
+{
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public override string ToString()
+        {
+            return "Color: " + Color + ", Area: " + Area().ToString("F2", CultureInfo.InvariantCulture) + ", Raio: " + Radius;
+        }
+    }
+}
+
+//Como ele herda da classe shape ele tem color. 
+//Como Shape implementa a classe abstrata Area, Circle tem que implementar
---
+++ Course/Entities/Model/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities.Model
+{
+    class Rectangle : Shape
+    {
+        public double Altura { get; set; }
+        public double Largula { get; set; }
+        public override double Area()
+        {
+            return Altura * Largula;
+        }
+
+        public override string ToString()
+        {
+            return "Color: " + Color + ", Area: " + Area().ToString("F2", CultureInfo.InvariantCulture) + ", Altura: " + Altura + ", Largura: " + Largula;
+        }
+    }
+}
